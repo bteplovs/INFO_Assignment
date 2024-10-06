4e034705ac720dc564e58656532446f9771144dd
Bogdans Teplovs <166667397+bteplovs@users.noreply.github.com>
2024-10-06 20:00:13 +1300

    Add files via upload

---
+++ bte32-Homework2/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace bte32_Homework2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblCounter = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbxCruisesFound = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbxShipName = new System.Windows.Forms.ComboBox();
+            this.dataGridViewCruiseInfo = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblTotalSelected = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCruiseInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(539, 113);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(108, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 84);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Cruise\r\nExplorer\r\n";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(99, 86);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Teal;
+            this.panel2.Controls.Add(this.lblCounter);
+            this.panel2.Controls.Add(this.btnExit);
+            this.panel2.Controls.Add(this.btnClear);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.cbxCruisesFound);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.cbxShipName);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 113);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(206, 316);
+            this.panel2.TabIndex = 1;
+            // 
+            // lblCounter
+            // 
+            this.lblCounter.AutoSize = true;
+            this.lblCounter.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCounter.ForeColor = System.Drawing.Color.White;
+            this.lblCounter.Location = new System.Drawing.Point(149, 90);
+            this.lblCounter.Name = "lblCounter";
+            this.lblCounter.Size = new System.Drawing.Size(19, 19);
+            this.lblCounter.TabIndex = 8;
+            this.lblCounter.Text = "0";
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.Black;
+            this.btnExit.Location = new System.Drawing.Point(12, 266);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(180, 38);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnClear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClear.FlatAppearance.BorderSize = 0;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.Color.Black;
+            this.btnClear.Location = new System.Drawing.Point(12, 213);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(180, 38);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(8, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Unique Cruises:";
+            // 
+            // cbxCruisesFound
+            // 
+            this.cbxCruisesFound.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.cbxCruisesFound.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxCruisesFound.FormattingEnabled = true;
+            this.cbxCruisesFound.Location = new System.Drawing.Point(12, 121);
+            this.cbxCruisesFound.Name = "cbxCruisesFound";
+            this.cbxCruisesFound.Size = new System.Drawing.Size(180, 24);
+            this.cbxCruisesFound.TabIndex = 3;
+            this.cbxCruisesFound.SelectedIndexChanged += new System.EventHandler(this.cbxCruisesFound_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(8, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ship name:";
+            // 
+            // cbxShipName
+            // 
+            this.cbxShipName.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.cbxShipName.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxShipName.FormattingEnabled = true;
+            this.cbxShipName.Items.AddRange(new object[] {
+            "Aurora",
+            "Arcadia",
+            "Adonia",
+            "Oceana",
+            "Grand Princess",
+            "Oriana",
+            "Dawn Princess"});
+            this.cbxShipName.Location = new System.Drawing.Point(12, 38);
+            this.cbxShipName.Name = "cbxShipName";
+            this.cbxShipName.Size = new System.Drawing.Size(180, 27);
+            this.cbxShipName.TabIndex = 0;
+            this.cbxShipName.SelectedIndexChanged += new System.EventHandler(this.cbxShipName_SelectedIndexChanged);
+            // 
+            // dataGridViewCruiseInfo
+            // 
+            this.dataGridViewCruiseInfo.AllowUserToAddRows = false;
+            this.dataGridViewCruiseInfo.AllowUserToDeleteRows = false;
+            this.dataGridViewCruiseInfo.AllowUserToResizeColumns = false;
+            this.dataGridViewCruiseInfo.AllowUserToResizeRows = false;
+            this.dataGridViewCruiseInfo.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewCruiseInfo.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.Disable;
+            this.dataGridViewCruiseInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCruiseInfo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridViewCruiseInfo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.dataGridViewCruiseInfo.Location = new System.Drawing.Point(215, 119);
+            this.dataGridViewCruiseInfo.Name = "dataGridViewCruiseInfo";
+            this.dataGridViewCruiseInfo.RowHeadersVisible = false;
+            this.dataGridViewCruiseInfo.Size = new System.Drawing.Size(315, 277);
+            this.dataGridViewCruiseInfo.TabIndex = 2;
+            this.dataGridViewCruiseInfo.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewCruiseInfo_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Start Date";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Duration";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Cost";
+            this.Column3.Name = "Column3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.GhostWhite;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(211, 399);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 19);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Total Cruises :";
+            // 
+            // lblTotalSelected
+            // 
+            this.lblTotalSelected.AutoSize = true;
+            this.lblTotalSelected.BackColor = System.Drawing.Color.GhostWhite;
+            this.lblTotalSelected.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalSelected.ForeColor = System.Drawing.Color.Black;
+            this.lblTotalSelected.Location = new System.Drawing.Point(352, 399);
+            this.lblTotalSelected.Name = "lblTotalSelected";
+            this.lblTotalSelected.Size = new System.Drawing.Size(19, 19);
+            this.lblTotalSelected.TabIndex = 10;
+            this.lblTotalSelected.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GhostWhite;
+            this.ClientSize = new System.Drawing.Size(539, 429);
+            this.Controls.Add(this.lblTotalSelected);
+            this.Controls.Add(this.dataGridViewCruiseInfo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(555, 468);
+            this.MinimumSize = new System.Drawing.Size(555, 468);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCruiseInfo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridViewCruiseInfo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.ComboBox cbxShipName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbxCruisesFound;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblCounter;
+        private System.Windows.Forms.Label lblTotalSelected;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ bte32-Homework2/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace bte32_Homework2
+{
+    public partial class Form1 : Form
+    {
+        
+        private static Dictionary<string, int> CRUISE_DICTIONARY = new Dictionary<string, int>
+        {
+            { "Aurora", 13 },
+            { "Arcadia", 15 },
+            { "Adonia", 16 },
+            { "Oceana", 17 },
+            { "Grand Princess", 18 },
+            { "Oriana", 20 },
+            { "Dawn Princess", 23 }
+        };
+            
+        int cruiseValue;
+        string[] Data;
+        List<string> shipData;
+        List<string> cruiseData;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ReadCruiseFile();
+        }
+        private void cbxShipName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Selected();
+            CruisesOfSelectedShip(); 
+            LoadCruises();
+            // essential polishing so that when a ship is changed the grid, cruise, and counters set back to default
+            dataGridViewCruiseInfo.Rows.Clear();
+            cbxCruisesFound.Text = "";
+            lblTotalSelected.Text = "0";
+        }
+
+        private void cbxCruisesFound_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            InfoOfSelectedCruise();
+            LoadInfo();
+        }
+
+        private void dataGridViewCruiseInfo_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Click on one of the available cruises for a message box prompt to confirm the selection
+            // call the datagrid object with forms grid and use the forms current state to fill a message box with the selected data
+            if (e.RowIndex >= 0) { 
+            
+            DataGridViewRow selectedRow = dataGridViewCruiseInfo.Rows[e.RowIndex];
+
+                string ship = cbxShipName.Text;
+                string cruise = cbxCruisesFound.Text;
+                string date = selectedRow.Cells[0].Value.ToString();
+                string duration = selectedRow.Cells[1].Value.ToString();
+                string cost = selectedRow.Cells[2].Value.ToString();
+
+                MessageBox.Show($"You selected the cruise: {cruise}\n" +
+                    $"Ship: {ship}\n" +
+                    $"Date: {date}\n" +
+                    $"Duration: {duration}\n" +
+                    $"Cost: {cost}\n" +
+                    "Would you like to confirm your selection?",
+                    "Cruise Selection",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question);
+            }
+            else
+            {
+                MessageBox.Show("Invalid Selection", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        /// 
+        ///  HELPER METHODS
+        /// 
+
+
+        private void ReadCruiseFile()
+        {
+            // loads the .txt
+            Data = File.ReadAllLines("Cruise.txt");
+        }
+
+        private void Selected()
+        {   // using key:val -> shipName:shipNum, finds the ships number. helps traverse the data
+            string selectedCruise = cbxShipName.Text;
+            if (CRUISE_DICTIONARY.TryGetValue(selectedCruise, out int newCruiseValue))
+            {
+                cruiseValue = newCruiseValue;
+            }
+            //else
+            //{
+                //cruiseValue = 0;
+            //}
+        }
+
+        private void CruisesOfSelectedShip()
+        {
+            // once a ship is selected initilise new list with only cruises offered by that ship
+            // this is done by appending the data items that share the ship num in index[1]
+            List<string> newShipData = new List<string>();
+
+            foreach (string line in Data)
+            {
+                string[] parts = line.Split(',');
+
+                if (parts[1].Trim() == cruiseValue.ToString())
+                {
+                    newShipData.Add(line);
+                }
+            }
+            shipData = newShipData;
+        }
+
+        private void LoadCruises()
+        {
+            // Loads each unique cruise into the combobox and tracks a count of each cruise added so the user knows how many unique cruises are available
+            cbxCruisesFound.Items.Clear();
+            int count = 0;
+            foreach (string line in shipData)
+            {
+                string[] parts = line.Split(',');
+
+                // if a cruise is not in the list add to cbx, else, add nothing
+                if (!cbxCruisesFound.Items.Contains(parts[5]))
+                { 
+                    cbxCruisesFound.Items.Add(parts[5]);
+                    count++;
+                }
+            }
+            lblCounter.Text = count.ToString();
+            // for each cruise a ship offers, add it to the combo box for user to select without duplicates and count the
+            // number of cruises available
+        }
+
+        private void InfoOfSelectedCruise()
+        {
+            // once a cruise is selected initilise new list with corresponding cruise dates/times/cost
+            // When a cruise type is selected, dates/times/costs are rewritten to cruisedata as to refresh with each selection
+            List<string> newCruiseData = new List<string>();
+
+            foreach (string line in shipData)
+            {
+                string[] parts = line.Split(',');
+
+                if (parts[5].Trim() == cbxCruisesFound.Text)
+                {
+                    newCruiseData.Add(line);
+                }
+            }
+            cruiseData = newCruiseData;
+            // cruiseData holds a list of a selected ship -> selected cruise 
+        }
+
+        private void LoadInfo()
+        {
+            // loads info into the datagridview by adding corresponding list index to column
+            // using the current instance of cruisedata, display dates/times/costs 
+            // from a selected ship --> selected cruise
+            dataGridViewCruiseInfo.Rows.Clear();
+            int count = 0;
+            foreach (string line in cruiseData)
+            {
+                string[] parts = line.Split(',');
+
+                string startDate = parts[4];
+                string duration = parts[2];
+                string costString = parts[3];
+                    
+                if (decimal.TryParse(costString, out decimal cost))
+                {
+                    dataGridViewCruiseInfo.Rows.Add(
+                    DateTime.Parse(startDate).ToString("MMMM dd, yyyy"),
+                    (duration + " days"),
+                    cost.ToString("C")
+                    );
+                    count++;
+                    // data is loaded by row, each iteration through a cruiseData adds start_date/duration/cost to the datagrid
+                }
+            }
+            lblTotalSelected.Text = count.ToString();
+        }
+        private void Clear()
+        {
+            // Clears all fields
+            cbxShipName.Text = "";
+            cbxCruisesFound.Items.Clear();
+            cbxCruisesFound.Text = "";
+            lblCounter.Text = "0";
+            lblTotalSelected.Text = "0";
+            dataGridViewCruiseInfo.Rows.Clear();
+        }
+    }
+}
+
+
+
